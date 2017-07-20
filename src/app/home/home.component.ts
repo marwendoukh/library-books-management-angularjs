ebf94278c36852a7688ea6d972136a1060d3c02f
Marwen Doukh <marwen.doukh@gmail.com>
2017-07-20 15:34:05 +0100

    search books module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  notFound=false
+  booksByName:Book[];
+  booksByISBN:Book[];
+
+
+
+  constructor(private dataService:DataService,private router: Router) { }
 
   ngOnInit() {
   }
+
+
+
+
+    searchBook(form: any): void {
+
+      this.dataService.findBookByName(form.searchtext).subscribe((result) => {
+
+      this.booksByName=result
+
+    });
+
+
+    this.dataService.findBookByISBN(form.searchtext).subscribe((result) => {
+
+      this.booksByISBN=result
+
+  });
+
+
+    }
+
+
+}
+
+
+interface Book{
+  name:string,
+  isbn:string,
+  category:string,
+  count:number,
+  createdAt:Date,
+  updatedAt:Date
 
 }
